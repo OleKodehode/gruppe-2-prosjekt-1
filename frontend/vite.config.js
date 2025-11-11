dd1507f5c3cb3ddcbf01c542a54e66679264b13d
Ole Kodehode <ole.kodehode@gmail.com>
2025-11-11 10:12:31 +0100

    gh-pages build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: mode === "production" ? "/gruppe-2-prosjekt-1/" : "/",
 });
